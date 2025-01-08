594f22a2ed22cf93cebebd92969bd6e6f321a5be
akAredminEogre <akAredminEogre@gmail.com>
2025-01-09 07:33:25 +0900

    feat: wxtプロジェクトを初期化

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:23.0-alpine3.19
+
+WORKDIR /opt/frontend-container-app-root/frontend-src-root
+
+# Chromiumのインストール
+RUN apk add --no-cache \
+  chromium
+
+# COPY ./host-frontend-root/frontend-src-root /opt/frontend-container-app-root/frontend-src-root
+
+# RUN npm install
---
+++ host-frontend-root/frontend-src-root/wxt.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  extensionApi: 'chrome',
+  modules: ['@wxt-dev/module-react'],
+
+  dev: {
+    server: {
+      hostname: 'localhost',
+      port: 3000,
+    }
+  },
+  runner: {
+    disabled: true,
+  },
+  vite:() =>  ({
+    server: {
+      host: 'localhost',
+      port: 3000,
+      strictPort: true, 
+      hmr: {
+        port: 3000,
+      }
+    }
+  }),
+});
